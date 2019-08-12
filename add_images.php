6407c5c823a1feac425aec71599b50d3aab7c8b3
sudhir <sudhir.rupani@peaas.co>
2019-08-13 01:05:18 +0530

    admin

---
+++ add_images.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+require_once 'config/config.php';
+require_once BASE_PATH.'/includes/auth_validate.php';
+
+// Serve POST method, After successful insert, redirect to customers.php page.
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    // Mass Insert Data. Keep "name" attribute in html form same as column name in mysql table.
+    $data_to_db = array_filter($_POST);
+
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}
+	}
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 50000000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}
+    $db = getDbInstance();
+    $last_id = $db->insert('gallery', $data_to_db);
+
+    if ($last_id)
+    {
+        $_SESSION['success'] = 'Customer added successfully!';
+        // Redirect to the listing page
+        header('Location: customers.php');
+        // Important! Don't execute the rest put the exit/die.
+    	exit();
+    }
+    else
+    {
+        echo 'Insert failed: ' . $db->getLastError();
+        exit();
+    }
+}
+
+// We are using same form for adding and editing. This is a create form so declare $edit = false.
+$edit = false;
+?>
+<?php include BASE_PATH.'/includes/header.php'; ?>
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Add Customer</h2>
+        </div>
+    </div>
+    <!-- Flash messages -->
+    <?php include BASE_PATH.'/includes/flash_messages.php'; ?>
+    <form action="" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+	</form>
+</div>
+<script type="text/javascript">
+$(document).ready(function(){
+   $('#customer_form').validate({
+       rules: {
+            f_name: {
+                required: true,
+                minlength: 3
+            },
+            l_name: {
+                required: true,
+                minlength: 3
+            },   
+        }
+    });
+});
+</script>
+<?php include BASE_PATH.'/includes/footer.php'; ?>
